4a89db2fc5f6da3dfcee21a1f180838d2d23a5a9
DanKalinin <daniil5511@gmail.com>
2015-08-03 13:00:08 +0300

    added .travis.yml

--- Example/Tests/Tests.m
+++ Example/Tests/Tests.m
@@ -1,48 +1,72 @@
 //
-//  URLTransactionTests.m
-//  URLTransactionTests
+//  AppTests.m
+//  AppTests
 //
-//  Created by DanKalinin on 07/31/2015.
-//  Copyright (c) 2015 DanKalinin. All rights reserved.
+//  Created by Dan Kalinin on 03.08.15.
+//  Copyright (c) 2015 Dan Kalinin. All rights reserved.
 //
 
-// https://github.com/Specta/Specta
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
 
-SpecBegin(InitialSpecs)
 
-describe(@"these will fail", ^{
 
-    it(@"can do maths", ^{
-        expect(1).to.equal(2);
-    });
-
-    it(@"can read", ^{
-        expect(@"number").to.equal(@"string");
-    });
+@interface URLRequestTests : XCTestCase {
+@private
+    int a;
+    int b;
     
-    it(@"will wait for 10 seconds and fail", ^{
-        waitUntil(^(DoneCallback done) {
-        
-        });
-    });
-});
+    NSString *first;
+    NSString *last;
+}
 
-describe(@"these will pass", ^{
-    
-    it(@"can do maths", ^{
-        expect(1).beLessThan(23);
-    });
-    
-    it(@"can read", ^{
-        expect(@"team").toNot.contain(@"I");
-    });
-    
-    it(@"will wait and succeed", ^{
-        waitUntil(^(DoneCallback done) {
-            done();
-        });
-    });
-});
+@end
 
-SpecEnd
 
+
+@implementation URLRequestTests
+
+- (void)setUp {
+    [super setUp];
+    
+    a = 20;
+    b = 5;
+    
+    first = @"John";
+    last = @"Taylor";
+}
+
+- (void)tearDown {
+    a = b = 0;
+    
+    first = last = nil;
+    
+    [super tearDown];
+}
+
+- (void)testAddition {
+    XCTAssertEqual(a + b, 25, @"Addition failed");
+}
+
+- (void)testSubtraction {
+    XCTAssertEqual(a - b, 15, @"Subtraction failed");
+}
+
+- (void)testDivision {
+    XCTAssertEqual(a / b, 4, @"Division failed");
+}
+
+- (void)testMultiplication {
+    XCTAssertEqual(a * b, 100, @"Multiplication failed");
+}
+
+- (void)testName {
+    XCTAssertNotNil(first, @"Empty first name");
+    XCTAssertNotNil(last, @"Empty last name");
+}
+
+- (void)testNameIsJohn {
+    XCTAssertEqualObjects(first, @"John", @"Name is not John");
+}
+
+@end
