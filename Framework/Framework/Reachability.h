41991c660b4a3a11246eea410d36575c15c48d75
Dan Kalinin <daniil5511@gmail.com>
2017-01-10 03:15:04 -0800

    reachability

---
+++ Framework/Framework/Reachability.h
@@ -0,0 +1,28 @@
+//
+//  Reachability.h
+//  Reachability
+//
+//  Created by Dan Kalinin on 1/9/17.
+//  Copyright © 2017 Dan Kalinin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef NS_ENUM(NSUInteger, ReachabilityStatus) {
+    ReachabilityStatusNone,
+    ReachabilityStatusWiFi,
+    ReachabilityStatusWWAN
+};
+
+
+
+@interface Reachability : NSObject
+
+typedef void (^ReachabilityStatusHandler)(ReachabilityStatus status);
+
+- (instancetype)initWithHost:(NSString *)host;
+
+@property (readonly) ReachabilityStatus status;
+@property (copy) ReachabilityStatusHandler statusHandler;
+
+@end
--- Framework/Framework/URLTransaction.h
+++ Framework/Framework/URLTransaction.h
@@ -13,6 +13,8 @@
 FOUNDATION_EXPORT double URLTransactionVersionNumber;
 FOUNDATION_EXPORT const unsigned char URLTransactionVersionString[];
 
+#import <URLTransaction/Reachability.h>
+
 extern NSErrorDomain const HTTPErrorDomain;
 
 extern NSString *const HTTPMethodOptions;
